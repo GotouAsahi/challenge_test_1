20da6691437555c8e66f0e80c823c3546408c718
smasa1112 <mtsakagami1112@yahoo.co.jp>
2022-05-15 06:06:01 +0000

    2_3問目の作問

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,13 +13,12 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      @books = Book.all
       render :index
     end
   end
 
   def show
-    @book = Book.find(params[:id])
+    book = Book.find(params[:id])
   end
 
   def edit
